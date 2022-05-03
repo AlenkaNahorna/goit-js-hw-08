32a7ccf637b91f7a5f41dea834c1c66ef6d4359d
AlenkaNahorna <agyloma@gmail.com>
2022-05-03 20:59:07 +0200

    finished second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function (data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const playedTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(Number(playedTime))
+  .then(function (seconds) {
+    seconds = Number(playedTime);
+    console.log(seconds);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('Time Error');
+        break;
+
+      default:
+        console.log('Other Error');
+        break;
+    }
+  });
