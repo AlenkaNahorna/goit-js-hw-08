b4cb93189e6fdd7cf6695e7dfef32aa9a0e12623
AlenkaNahorna <agyloma@gmail.com>
2022-05-04 21:41:02 +0200

    fixed bugs

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,37 +3,16 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+const currentTime = 'videoplayer-current-time';
 
 const onPlay = function (data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+  localStorage.setItem(currentTime, data.seconds);
 };
 
 player.on('timeupdate', throttle(onPlay, 1000));
 
-const playedTime = localStorage.getItem('videoplayer-current-time');
+const playedTime = localStorage.getItem(currentTime);
 
-player
-  .setCurrentTime(Number(playedTime))
-  .then(function (seconds) {
-    seconds = Number(playedTime);
-    console.log(seconds);
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        console.log('Time Error');
-        break;
-
-      default:
-        console.log('Other Error');
-        break;
-    }
-  });
+player.setCurrentTime(Number(playedTime)).then(function (seconds) {
+  seconds = Number(playedTime);
+});
